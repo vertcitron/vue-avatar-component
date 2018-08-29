4c605fe409bd34bbe9d6dba5b3ae361dfbdc7fc5
Nick Steele <njsteele@gmail.com>
2018-08-29 10:19:50 -0400

    Fixes undefined bug on no string passing toColor

--- Avatar.vue
+++ Avatar.vue
@@ -53,7 +53,7 @@
       toColor (str) {
         var hash = 0
         var len = str.length
-        if (len === 0) return black
+        if (len === 0) return 'black'
         for (var i = 0; i < len; i++) {
           hash = ((hash << 8) - hash) + str.charCodeAt(i)
           hash |= 0
